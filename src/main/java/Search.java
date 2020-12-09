40c99651c5d59b9fe975d835faa0b90e44d8ddcf
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-12-09 17:50:46 -0500

    lambdas branch done

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -3,12 +3,9 @@ import java.util.List;
 public class Search implements CheckPerson{
 
     public static void printPersons(List<Person> roster, CheckPerson tester) {
-        for (Person p : roster) {
-            if (tester.test(p)) {
-                p.printPerson();
-            }
-        }
+        roster.stream().filter(p -> tester.test(p)).forEach(p -> p.printPerson());
     }
+
 
     @Override
     public boolean test(Person p) {
